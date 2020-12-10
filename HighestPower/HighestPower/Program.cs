db535ce30bf262f05cf95efd0880e229a0d29c34
thetoon30 <thetoon30@gmail.com>
2020-12-10 08:59:47 +0700

    new method HighestPower

--- HighestPower/HighestPower/Program.cs
+++ HighestPower/HighestPower/Program.cs
@@ -12,15 +12,15 @@ namespace HighestPower
         {
             //The input range is from 1 to 1,000,000. 
             int n = 90;
-            
+            Console.WriteLine(n + " " + HighestPower(n));
             n = 65;
-           
+            Console.WriteLine(n + " " + HighestPower(n));
             n = 81;
-            
+            Console.WriteLine(n + " " + HighestPower(n));
             n = 29;
-           
+            Console.WriteLine(n + " " + HighestPower(n));
             n = 6;
-
+            Console.WriteLine(n + " " + HighestPower(n));
             Console.ReadKey();
         }
 
