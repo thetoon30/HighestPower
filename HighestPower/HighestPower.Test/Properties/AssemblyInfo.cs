412eb0f873c070fd4a2104ec8b198a7430ae6524
thetoon30 <thetoon30@gmail.com>
2020-12-12 09:10:09 +0700

    Unint test project

---
+++ HighestPower/HighestPower.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HighestPower.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HighestPower.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cca62f19-2888-4672-a810-a722ae14931c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HighestPower/HighestPower.Test/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HighestPower.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        [DataRow(90, 81, 2)]
+        [DataRow(6, 4, 1)]
+        [DataRow(65, 64, 3)]
+        [DataRow(3, 1, -1)]
+        [DataRow(1, 0, -1)]
+        [DataRow(81, 64, 3)]
+        [DataRow(29, 27, 1)]
+        [DataRow(4, 1, -1)]
+        public void CalculateTest(int def, int expected1 , int expected2)
+        {
+            
+            Tuple<int, int> actual = HighestPowerOccurrences.Calculate(def);
+
+            Assert.AreEqual(new Tuple<int, int>(expected1, expected2), actual);
+        }
+    }
+}
